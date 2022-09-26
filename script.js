8a8dd80e869276ef687dd0a819fa107053e45689
broothie <adbooth8@gmail.com>
2022-09-26 14:50:23 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const light = [] // #FCF0F8
+const dark = [] // #F7CBE8
+
+// function runFrame() {
+//
+//
+//   requestAnimationFrame(runFrame)
+// }
+//
+// document.addEventListener('DOMContentLoaded', () => {
+//   runFrame()
+// })
